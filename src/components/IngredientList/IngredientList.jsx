5effffa017ccf5837c7262aebafa4d6c7f00e819
suzanaai <angelska327@gmail.com>
2026-01-12 17:28:16 -0600

    working

---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,19 @@
+// src/components/IngredientList/IngredientList.jsx
+
+const IngredientList = ({ availableIngredients, addToBurger }) => {
+  return (
+    <ul>
+      {availableIngredients.map((ingredient, index) => (
+        <li
+          key={index}
+          style={{ backgroundColor: ingredient.color }}
+        >
+          {ingredient.name}
+          <button onClick={() => addToBurger(ingredient)}>+</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default IngredientList
